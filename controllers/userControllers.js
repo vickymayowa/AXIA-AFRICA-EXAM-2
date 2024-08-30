836f47c81cd645fb6f04f365c8c34325cbe8b4ea
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-08-30 20:22:01 +0100

    Worked on the middleware auth

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,12 +5,12 @@ const jwt = require("jsonwebtoken");
 // Register User
 exports.registerUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
 
-    const user = new User({ username, email, password });
+    const user = new User({ name, email, password });
     await user.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
--- models/postModel.js
+++ models/postModel.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  dislikes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-});
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", postSchema);
-
 module.exports = Post;
